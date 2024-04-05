7a8488a41ce14cfd4973a31ee38193bd3be5ba5c
salmonco <xkqjsslsek80@naver.com>
2024-04-05 20:59:27 +0900

    Design: tag button 컴포넌트 추가

---
+++ src/components/tagButton.js
@@ -0,0 +1,11 @@
+export default function TagButton({ label, active }) {
+  return (
+    <button
+      className={`shrink-0 min-w-[69px] h-[30px] shadow-gray px-[16px] rounded-[15px] font-[Pretendard-Medium] text-[1.4rem] ${
+        active ? "text-white bg-[#FF823C]" : "text-[#9DA0A8] bg-white"
+      }`}
+    >
+      {label}
+    </button>
+  );
+}
