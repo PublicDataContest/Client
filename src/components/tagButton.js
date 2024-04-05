a10348802963f8769ba36558528936a853310dad
salmonco <xkqjsslsek80@naver.com>
2024-04-05 22:33:37 +0900

    Feat: Add callbackFn onclick tag button

--- src/components/tagButton.js
+++ src/components/tagButton.js
@@ -1,9 +1,10 @@
-export default function TagButton({ label, active }) {
+export default function TagButton({ label, active, callbackFn }) {
   return (
     <button
       className={`shrink-0 min-w-[69px] h-[30px] shadow-gray px-[16px] rounded-[15px] font-[Pretendard-Medium] text-[1.4rem] ${
         active ? "text-white bg-[#FF823C]" : "text-[#9DA0A8] bg-white"
       }`}
+      onClick={callbackFn}
     >
       {label}
     </button>
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,8 +1,20 @@
 import BottomTabNav from "@component/components/bottomTabNav";
 import Card from "@component/components/card";
 import TagButton from "@component/components/tagButton";
+import { useState } from "react";
 
 export default function Home() {
+  const [selectedTag, setSelectedTag] = useState(1);
+  const TAG_MENU = [
+    { idx: 1, label: "거리순" },
+    { idx: 2, label: "착한가게" },
+    { idx: 3, label: "친환경" },
+    { idx: 4, label: "친환경" },
+    { idx: 5, label: "친환경" },
+    { idx: 6, label: "친환경" },
+    { idx: 7, label: "친환경" },
+  ];
+
   return (
     <div className="relative h-[100vh]">
       <div className="absolute top-0 left-0 w-[375px] pt-[44px]">
@@ -10,13 +22,14 @@ export default function Home() {
           <input className="w-[343px] h-[44px] pl-[38px] pr-[12px] px-[12px] text-[1.4rem] text-[#3B3F4A] bg-white rounded-[10px] shadow-gray" />
         </div>
         <div className="pl-[16px] py-[10px] flex gap-[8px] overflow-x-auto scrollbar-hide">
-          <TagButton label="거리순" active={true} />
-          <TagButton label="착한가게" active={false} />
-          <TagButton label="친환경" active={false} />
-          <TagButton label="친환경" active={false} />
-          <TagButton label="친환경" active={false} />
-          <TagButton label="친환경" active={false} />
-          <TagButton label="친환경" active={false} />
+          {TAG_MENU.map((v) => (
+            <TagButton
+              key={v.idx}
+              label={v.label}
+              active={v.idx === selectedTag}
+              callbackFn={() => setSelectedTag(v.idx)}
+            />
+          ))}
         </div>
       </div>
 
