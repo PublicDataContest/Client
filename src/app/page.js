18cd78d5874e507da854e98a87dcdfd9bc07c75e
salmonco <xkqjsslsek80@naver.com>
2024-04-28 14:23:52 +0900

    Design: 처음 접근 페이지

--- src/app/page.js
+++ src/app/page.js
@@ -1,19 +1,72 @@
+"use client";
 import Link from "next/link";
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex flex-col justify-between items-center p-[20px] h-[100vh]">
-      <div className="flex flex-col items-center pt-[200px]">
-        <span className="font-[Pretendard-Medium] text-[1.4rem] text-[#333333]">
-          공무원 맛집 찾기
-        </span>
-        <span className="font-[Pretendard-Bold] text-[4.2rem]">HELLO!</span>
+    <main className="bg-brand flex flex-col justify-around p-[42px] h-[100vh]">
+      <div className="flex flex-col gap-[12px]">
+        <Image
+          alt=""
+          src={require("@images/symbol.svg")}
+          width={70}
+          height={70}
+          priority
+          className="shadow-gray"
+        />
+        <p className="font-b text-[2.2rem] text-[#F9FAFC]">
+          간편 로그인으로 공식탐방을
+          <br />
+          즐겨보세요!
+        </p>
       </div>
-      <Link className="w-full" href="/home">
-        <button className="w-full disabled:bg-[#E5E5E5] disabled:text-[#999999] bg-[#FF823C] text-white text-[1.4rem] font-[Pretendard-Bold] h-[53px] rounded-[15px]">
-          시작하기
+      <div className="flex flex-col gap-[12px] items-center">
+        {/* <Link className="w-full" href="/home"> */}
+        <button className="bg-[#FEE500] flex items-center gap-[34px] px-[46px] shadow-gray-sm w-full text-[1.4rem] font-m h-[42px] rounded-[5px]">
+          <Image
+            alt=""
+            src={require("@images/kakao.svg")}
+            width={24}
+            height={24}
+          />
+          <span className="text-[#3B3F4A]">카카오톡으로 로그인</span>
         </button>
-      </Link>
+        {/* </Link> */}
+
+        {/* <Link className="w-full" href="/home"> */}
+        <button className="bg-[#03C75A] flex items-center gap-[34px] px-[46px] shadow-gray-sm w-full text-[1.4rem] font-m h-[42px] rounded-[5px]">
+          <Image
+            alt=""
+            src={require("@images/naver.svg")}
+            width={24}
+            height={24}
+          />
+          <span className="text-white">네이버로 로그인</span>
+        </button>
+        {/* </Link> */}
+
+        {/* <Link className="w-full" href="/home"> */}
+        <button className="bg-[#F9FAFC] flex items-center gap-[34px] px-[46px] shadow-gray-sm w-full text-[1.4rem] font-m h-[42px] rounded-[5px]">
+          <Image
+            alt=""
+            src={require("@images/google.svg")}
+            width={24}
+            height={24}
+          />
+          <span className="text-[#555]">구글로 로그인</span>
+        </button>
+        {/* </Link> */}
+
+        <div className="flex gap-[10px] items-center font-m text-[1.3rem] text-[#F9FAFC]">
+          <Link href={"/login"}>
+            <span>로그인</span>
+          </Link>
+          <div className="w-[1px] h-[11px] bg-[#F9FAFC]" />
+          <Link href={"/signup"}>
+            <span>회원가입</span>
+          </Link>
+        </div>
+      </div>
     </main>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,7 @@ module.exports = {
       boxShadow: {
         gray: "0px 2px 6px 0px rgba(0, 0, 0, 0.12)",
         "t-gray": "0px -4px 8px 0px rgba(0, 0, 0, 0.12)",
+        "gray-sm": "0px 2px 4px 0px rgba(0, 0, 0, 0.06)",
       },
       content: {
         locationIcon: 'url("/images/location-orange.svg")',
@@ -17,6 +18,15 @@ module.exports = {
         starRoundGrayIcon: 'url("/images/star-round-gray.svg")',
         searchIcon: 'url("/images/search-gray.svg")',
         barGrayIcon: 'url("/images/bar-gray.svg")',
+      },
+      backgroundColor: {
+        brand: "#FF823C",
+      },
+      fontFamily: {
+        r: "Pretendard-Regular",
+        m: "Pretendard-Medium",
+        sb: "Pretendard-SemiBold",
+        b: "Pretendard-Bold",
       },
     },
   },
