0c2de31216e223a5258bcc39a890f394e5113484
salmonco <xkqjsslsek80@naver.com>
2024-05-01 14:41:08 +0900

    Feat: refresh token

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -21,7 +21,12 @@ axiosInstance.interceptors.response.use(
   },
   async function (error) {
     const originalRequest = error.config;
-    if (error.response.status === 403 && !originalRequest._retry) {
+    if (error.response.status === 401 && !originalRequest._retry) {
+      if (!localStorage.getItem("refreshToken")) {
+        alert("로그인이 필요한 페이지입니다.");
+        location.href = "/";
+        return;
+      }
       originalRequest._retry = true;
       const accessToken = await refreshAccessToken();
       axios.defaults.headers.common["Authorization"] = `Bearer ${accessToken}`;
@@ -30,5 +35,32 @@ axiosInstance.interceptors.response.use(
     return Promise.reject(error);
   }
 );
+
+export async function refreshAccessToken() {
+  try {
+    const res = await axios.post(
+      "/api/api/refresh-token",
+      {
+        refreshToken: localStorage.getItem("refreshToken"),
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+        },
+      }
+    );
+    console.log("refresh-token", res);
+    const { accessToken, refreshToken, userId } = res.data;
+    localStorage.setItem("accessToken", accessToken);
+    localStorage.setItem("refreshToken", refreshToken);
+    localStorage.setItem("userId", userId);
+  } catch (error) {
+    if (error.response.status === 401) {
+      alert("세션이 만료되어 로그인 페이지로 이동합니다.");
+      location.href = "/";
+      return;
+    }
+  }
+}
 
 export default axiosInstance;
