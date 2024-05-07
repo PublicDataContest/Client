b8f21f930186feca83515f8e468bf961f3e88c13
salmonco <xkqjsslsek80@naver.com>
2024-05-08 01:38:15 +0900

    Design: 첫화면에 개인정보처리방침 링크 표시

--- src/app/page.js
+++ src/app/page.js
@@ -32,16 +32,23 @@ export default function Home() {
           즐겨보세요!
         </p>
       </div>
-      <div className="flex flex-col gap-[18px] items-center">
-        <Link className="w-full" href="/login">
-          <button className="bg-white shadow-gray-sm w-full text-[1.4rem] font-m h-[42px] rounded-[5px]">
-            <span className="text-[#3B3F4A]">로그인</span>
-          </button>
-        </Link>
-        <Link className="w-full" href="/signup">
-          <button className="bg-[#464343] shadow-gray-sm w-full text-[1.4rem] font-m h-[42px] rounded-[5px]">
-            <span className="text-white">회원가입</span>
-          </button>
+      <div className="flex flex-col gap-[8px] items-center">
+        <div className="w-full flex flex-col gap-[18px] items-center">
+          <Link className="w-full" href="/login">
+            <button className="bg-white shadow-gray-sm w-full text-[1.4rem] font-m h-[42px] rounded-[5px]">
+              <span className="text-[#3B3F4A]">로그인</span>
+            </button>
+          </Link>
+          <Link className="w-full" href="/signup">
+            <button className="bg-[#464343] shadow-gray-sm w-full text-[1.4rem] font-m h-[42px] rounded-[5px]">
+              <span className="text-white">회원가입</span>
+            </button>
+          </Link>
+        </div>
+        <Link href="/privacy" target="blank">
+          <span className="font-b text-[1.3rem] text-[#3B3F4A]">
+            개인정보처리방침
+          </span>
         </Link>
       </div>
     </main>
